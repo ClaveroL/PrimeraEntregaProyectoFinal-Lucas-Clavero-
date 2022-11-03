d9071a55a099211846cbfa40bcc0d0bef6d8f117
ClaveroL <97352931+ClaveroL@users.noreply.github.com>
2022-11-03 11:05:42 -0300

    a
    a

--- ProyectoFinal LucasClavero/Assets/MovimientoPlayer1.cs
+++ ProyectoFinal LucasClavero/Assets/MovimientoPlayer1.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -5,15 +6,11 @@ using UnityEngine;
 public class MovimientoPlayer1 : MonoBehaviour
 {
     public Rigidbody RbPlayer;
-    public float Speedmovimiento;
+    float Speedmovimiento;
     public float JumpForce;
     bool Floor = false;
+    float Cooldown = 0f;
 
-  
-    void Start()
-    {
-        
-    }
     void FixedUpdate()
     {
         MovimientoPlayer();
@@ -23,18 +20,18 @@ public class MovimientoPlayer1 : MonoBehaviour
     {
         float x = Input.GetAxis("Horizontal");
         float z = Input.GetAxis("Vertical");
-        RbPlayer.AddForce(x*Speedmovimiento,0,z*Speedmovimiento);
+        RbPlayer.AddForce(x * Speedmovimiento, 0, z * Speedmovimiento);
         if (x > 0)
         {
-            transform.rotation = Quaternion.Euler(0, 90, 0);
+            transform.rotation = Quaternion.Euler(0, 90, 0);           
         }
         if (x < 0)
         {
-            transform.rotation = Quaternion.Euler(0, 270, 0);
+            transform.rotation = Quaternion.Euler(0, 270, 0);        
         }
         if (z > 0)
         {
-            transform.rotation = Quaternion.Euler(0, 0, 0);
+            transform.rotation = Quaternion.Euler(0, 0, 0);           
         }
         if (z < 0)
         {
@@ -56,14 +53,24 @@ public class MovimientoPlayer1 : MonoBehaviour
         {
             transform.rotation = Quaternion.Euler(0, 135, 0);
         }
-        if (Physics.Raycast(transform.position, transform.TransformDirection(Vector3.down), 0.5f))
+        if (Physics.Raycast(transform.position,Vector3.down, 1f))
         {
             Floor = true;
+            Cooldown -= Time.deltaTime;
+            Speedmovimiento = 150;
+
         }
-        if(Floor == true && Input.GetKeyDown(KeyCode.Space)) 
+        else 
         {
-            RbPlayer.AddForce(Vector3.up*JumpForce);
+            Floor = false;
+            Speedmovimiento = 70;
+        }
+        if(Floor == true && Input.GetKey(KeyCode.X) && Cooldown < 0) 
+        {
+            RbPlayer.AddForceAtPosition(Vector3.up * JumpForce, transform.position, ForceMode.Impulse);
+            Cooldown = 0.1f;
         }
 
+        Debug.Log(Speedmovimiento);
     }
 }
